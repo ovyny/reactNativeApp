bdbd597e848d41b642354d18d53b7b0112c1f072
ovyny <ovyny@mac-mini-de-ovyny.home>
2018-02-04 02:25:53 +0100

    simple album app with linking api and api data

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import a library to help build a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it on device
+AppRegistry.registerComponent('reactNativeApp', () => App);
